b9bf2a11a8199d37942644041fc5c1e36ffd20df
sanyam <gumbersanyam@gmail.com>
2018-05-29 16:49:22 +0530

    Update Build.ps1

--- Build.ps1
+++ Build.ps1
@@ -5,4 +5,6 @@ echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
 echo "Powershell Workspace is $env:WORKSPACE"
 echo "UserFlag is $env:userFlag"
 echo "Choice is $env:Choice"
+echo "Choice is $Choice"
+echo "Choice is ${params.Choice}"
 Copy-Item -Path "D:\Config\*" -Destination "D:\PACKAGES\" -Recurse -Force -ErrorAction Stop
