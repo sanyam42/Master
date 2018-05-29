897c7d66860aa671d012723c44d903bf92e9bdbf
sanyam <gumbersanyam@gmail.com>
2018-05-29 16:59:49 +0530

    Update Build.ps1

--- Build.ps1
+++ Build.ps1
@@ -1,11 +1,10 @@
 echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
 Write-Host "SONARQUBE NOT SELECTED"
 echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
-#New-Item -ItemType directory -Path D:\PACKAGE -ErrorAction Stop
+New-Item -ItemType directory -Path D:\PACKAGE -ErrorAction Stop
 echo "Powershell Workspace is $env:WORKSPACE"
 echo "UserFlag is $env:userFlag"
 echo "Choice is $env:Choice"
-echo "Choice 1 is $Choice"
-echo "Choice 2 is ${params.Choice}"
-echo "Powershell Workspace is ${workspace}"
+echo "Build No.is $env:BUILD_NUMBER"
+
 Copy-Item -Path "D:\Config\*" -Destination "D:\PACKAGES\" -Recurse -Force -ErrorAction Stop
