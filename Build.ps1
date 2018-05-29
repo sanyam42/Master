5c9169f9476f9b526666c1da3c2f68bef7aada1e
sanyam <gumbersanyam@gmail.com>
2018-05-29 15:14:57 +0530

    Update Build.ps1

--- Build.ps1
+++ Build.ps1
@@ -2,6 +2,6 @@ echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
 Write-Host "SONARQUBE NOT SELECTED"
 echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
 New-Item -ItemType directory -Path D:\PACKAGE -ErrorAction Stop
-echo "$env.Workspace"
-echo "$env.userFlag"
+echo "Workspace is $env.Workspace"
+echo "UserFlag is $env.userFlag"
 Copy-Item -Path "D:\Config\*" -Destination "D:\PACKAGES\" -Recurse -Force -ErrorAction Stop
