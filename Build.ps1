586390355b8066008c68c3aa2e885bef9c715e85
sanyam <gumbersanyam@gmail.com>
2018-05-29 11:31:50 +0530

    Update Build.ps1

--- Build.ps1
+++ Build.ps1
@@ -1,3 +1,5 @@
-﻿echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
+echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
 Write-Host "SONARQUBE NOT SELECTED"
 echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
+New-Item -ItemType directory -Path D:\PACKAGE -ErrorAction Stop
+Copy-Item -Path "D:\Config\*" -Destination "D:\PACKAGES\" -Recurse -Force -ErrorAction Stop
