2822f90485bfb59ed8d8ace5c72f48d0ad8dc774
sanyam <gumbersanyam@gmail.com>
2018-06-01 11:24:02 +0530

    Update Build.ps1

--- Build.ps1
+++ Build.ps1
@@ -6,5 +6,6 @@ echo "Powershell Workspace is $env:WORKSPACE"
 echo "UserFlag is $env:userFlag"
 echo "Choice is $env:Choice"
 echo "Build No.is $env:BUILD_NUMBER" ; exit 1
+echo "Not exiting 1"
 
 Copy-Item -Path "D:\Config\*" -Destination "D:\PACKAGES\" -Recurse -Force -ErrorAction Stop
