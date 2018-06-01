4e09ea920d576105175fff0cee46aad1cf29c11c
sanyam <gumbersanyam@gmail.com>
2018-06-01 11:38:29 +0530

    Update Build.ps1

--- Build.ps1
+++ Build.ps1
@@ -5,7 +5,11 @@ New-Item -ItemType directory -Path D:\PACKAGE -ErrorAction SilentlyContinue
 echo "Powershell Workspace is $env:WORKSPACE"
 echo "UserFlag is $env:userFlag"
 echo "Choice is $env:Choice"
-echo "Build No.is $env:BUILD_NUMBER" ; exit 1
-echo "Not exiting 1"
+echo "Build No.is $env:BUILD_NUMBER" 
+
+if ($? -eq "True")
+{Exiting 1 } ; exit 1
+else
+   {echo "Not exiting 1"}
 
 Copy-Item -Path "D:\Config\*" -Destination "D:\PACKAGES\" -Recurse -Force -ErrorAction Stop
